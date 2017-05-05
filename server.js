6cc05d6ec23372b1a58f583a7e7c381fa90fa41e
deadlybutter <thedeadlybutter@gmail.com>
2017-05-05 14:29:27 -0400

    gating info

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 
+const users = {};
+users[process.env.WEB_USER] = process.env.WEB_PASSWORD;
+
+const basicAuth = require('express-basic-auth');
+app.use(basicAuth({ users, challenge: true }));
+
 app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(process.env.PORT || 5000, () => {});
+const port = process.env.PORT || 5000;
+app.listen(port, () => { console.info(`listening on ${port}`) });
